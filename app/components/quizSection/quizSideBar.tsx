aed292ee3b97bb35e5aae680366fddf145fae2c5
Robbie Booth CS2021 <robbie.booth.2021@uni.strath.ac.uk>
2024-12-21 18:07:46 +0000

    inital Commits
    inital Commits

---
+++ app/components/quizSection/quizSideBar.tsx
@@ -0,0 +1,105 @@
+import {Calendar, Home, Inbox, Search, Settings, LogOut, BookOpen, Flag, FlagOff} from "lucide-react";
+
+import {
+    Sidebar,
+    SidebarContent, SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel, SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "~/components/ui/sidebar";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "~/components/ui/carousel";
+import {Card, CardContent} from "~/components/ui/card";
+import {Button} from "~/components/ui/button";
+import {Question} from "~/routes/about";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@radix-ui/react-tooltip";
+import React, {Component} from "react";
+
+export function FlagManager({isFlagged, setFlagged}:{ isFlagged:boolean, setFlagged:(flag: boolean) => void}) {
+    const [isHovering, setIsHovering] = React.useState(false);
+
+    const flagCSS = "absolute right-4";
+
+    return (isFlagged && <TooltipProvider delayDuration={0}>
+        <Tooltip>
+            <TooltipTrigger asChild onMouseOver={() => setIsHovering(true)} onMouseOut={() => setIsHovering(false)} onClick={()=> setFlagged(!isFlagged)}>
+                {isHovering ? <FlagOff className={flagCSS} /> : <Flag className={flagCSS}/>}
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>Unflag</p>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>);
+}
+
+export function AppSidebar({questions, currentQuestion, setCurrentQuestion, setFlagged}: {questions: Question[], currentQuestion: Question, setCurrentQuestion: (question: Question) => void, setFlagged: (question: Question, flag:boolean) => void} ){
+    return (
+        <Sidebar className="h-screen">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem key={"LeaveQuiz"}>
+                        <SidebarMenuButton asChild>
+                            <a href={"#"}>
+                                <LogOut className="rotate-180" />
+                                <span>Leave Quiz</span>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>Questions</SidebarGroupLabel>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            <Carousel
+                                opts={{
+                                    align: "start",
+                                }}
+                                orientation="vertical"
+                                className="w-full max-w-xs"
+                            >
+                                <CarouselContent className="-mt-2 h-full">
+                                    {questions.map((question, index) => (
+                                        <CarouselItem key={index} className="pt-1 md:basis-1/2">
+                                            <div className="p-1">
+                                                <Card>
+                                                    <CardContent
+                                                        className="relative flex items-center justify-center p-6 ">
+                                                        <Button variant="secondary"
+                                                                className={`text-3xl font-semibold ${(currentQuestion.uuid === question.uuid ? "underline" : "")}`}
+                                                                onClick={() => setCurrentQuestion(question)}>{index + 1}</Button>
+                                                        <FlagManager isFlagged={question.isFlagged} setFlagged={(flag:boolean) => setFlagged(question, flag)}/>
+
+                                                    </CardContent>
+                                                </Card>
+                                            </div>
+                                        </CarouselItem>
+                                    ))}
+                                </CarouselContent>
+                                {/*<CarouselPrevious />*/}
+                                {/*<CarouselNext />*/}
+                            </Carousel>
+
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem key={"ReviewQuiz"}>
+                        <SidebarMenuButton asChild>
+                            <a href={"#"}>
+                                <BookOpen />
+                                <span>Review Quiz</span>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
+
--- app/root.tsx
+++ app/root.tsx
@@ -5,12 +5,31 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type {LinksFunction, LoaderFunctionArgs} from "@remix-run/node";
 
 import "./tailwind.css";
+import styles from "./tailwind.css?url";
+
+import clsx from "clsx"
+import { PreventFlashOnWrongTheme, ThemeProvider, useTheme } from "remix-themes"
+
+import { themeSessionResolver } from "./session.server";
+import { useLoaderData } from "@remix-run/react";
+
+// Return the theme from the session storage using the loader
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { getTheme } = await themeSessionResolver(request)
+  return {
+    theme: getTheme(),
+  }
+}
+// Wrap your app with ThemeProvider.
+// `specifiedTheme` is the stored theme in the session storage.
+// `themeAction` is the action name that's used to change the theme in the session storage.
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: "stylesheet", href: styles },
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
     href: "https://fonts.gstatic.com",
@@ -22,17 +41,21 @@ export const links: LinksFunction = () => [
   },
 ];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function App({ children }: { children: React.ReactNode }) {
+  const data = useLoaderData<typeof loader>()
+  const [theme] = useTheme()
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
+        <PreventFlashOnWrongTheme ssrTheme={Boolean(data.theme)} />
         <Links />
       </head>
       <body>
         {children}
+        <Outlet />
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -40,6 +63,18 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+// export default function App() {
+//   return <Outlet />;
+// }
+
+// Wrap your app with ThemeProvider.
+// `specifiedTheme` is the stored theme in the session storage.
+// `themeAction` is the action name that's used to change the theme in the session storage.
+export default function AppWithProviders() {
+  const data = useLoaderData<typeof loader>()
+  return (
+      <ThemeProvider specifiedTheme={data.theme} themeAction="/action/set-theme">
+        <App children={[]}/>
+      </ThemeProvider>
+  )
 }
