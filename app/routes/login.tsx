99f7370708430278052ae104165be3c7760d3cf3
Robbie Booth CS2021 <robbie.booth.2021@uni.strath.ac.uk>
2025-02-07 15:34:45 +0000

    Made login page more professional

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,6 +4,8 @@
 import {LoaderFunctionArgs, redirect} from "@remix-run/node";
 import {Route} from "react-router";
 import {sessionStorage} from "~/session.server";
+import {GalleryVerticalEnd} from "lucide-react";
+import {LoginForm} from "~/components/login/loginForm";
 
 export async function loader({ request }: LoaderFunctionArgs) {
     const session = await sessionStorage.getSession(request.headers.get("cookie"));
@@ -14,11 +16,25 @@ export async function loader({ request }: LoaderFunctionArgs) {
 }
 
 export default function Login() {
+    // return (
+    //     <div>
+    //         <h1>Login</h1>
+    //         <a href="/auth/github/">Login with GitHub</a>
+    //         <a href="/auth/microsoft/">Login with Microsoft</a>
+    //     </div>
+    // );
     return (
-        <div>
-            <h1>Login</h1>
-            <a href="/auth/github/">Login with GitHub</a>
-            <a href="/auth/microsoft/">Login with Microsoft</a>
+        <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <a href="#" className="flex items-center gap-2 self-center font-medium">
+                    <div
+                        className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+                        <GalleryVerticalEnd className="size-4"/>
+                    </div>
+                    ME Environment.
+                </a>
+                <LoginForm/>
+            </div>
         </div>
-    );
+    )
 }
